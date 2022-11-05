644e7d253f201f736cf297709516fd71fa81b53c
nico-loy <nico.loy@me.com>
2022-11-05 13:57:16 +0100

    1. Sitzung zuhause

--- app/src/main/java/at/fh/mappdev/loggingviewsandactivity/LessonAdapter.kt
+++ app/src/main/java/at/fh/mappdev/loggingviewsandactivity/LessonAdapter.kt
@@ -3,12 +3,14 @@ package at.fh.mappdev.loggingviewsandactivity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import java.nio.file.Files.size
 
 class LessonAdapter: RecyclerView.Adapter<LessonViewHolder>() {
 
     private var lessonList = listOf<Lesson>()
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LessonViewHolder {
         val inflater = LayoutInflater.from(parent.context)
@@ -18,18 +20,26 @@ class LessonAdapter: RecyclerView.Adapter<LessonViewHolder>() {
 
     override fun getItemCount(): Int {
 
-            return lessonList.size
+        return lessonList.size
 
         //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun onBindViewHolder(holder: LessonViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        //TODO get the correct lesson Object and pass it to bindItem
+        holder.bindItem(lessonList[position])
+    }
+
+    fun updateList(newList: List<Lesson>) {
+        lessonList = newList
+        notifyDataSetChanged()
     }
 }
 
 class LessonViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
     fun bindItem(lesson: Lesson) {
+        itemView.findViewById<TextView>(R.id.item_lesson_name).text = lesson.name
+        itemView.findViewById<TextView>(R.id.item_lesson_date).text = lesson.date
 
     }
 }
--- app/src/main/java/at/fh/mappdev/loggingviewsandactivity/LessonListActivity.kt
+++ app/src/main/java/at/fh/mappdev/loggingviewsandactivity/LessonListActivity.kt
@@ -2,10 +2,19 @@ package at.fh.mappdev.loggingviewsandactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class LessonListActivity : AppCompatActivity() {
+    val lessonAdapter = LessonAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lesson_list)
+
+        lessonAdapter.updateList(LessonRepository.lessonsList())
+        val recyclerView = findViewById<RecyclerView>(R.id.lesson_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = lessonAdapter
     }
 }
